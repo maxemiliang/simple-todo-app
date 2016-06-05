8f9eb8e59713cd354e50354656ae88dec1d7f24f
Maxemilian <maxemiliang@gmail.com>
2016-06-05 20:04:59 +0300

    added some frontend stuff

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var hbs = require('hbs');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT || 1337, (err) => {
+  if (err) throw err;
+  console.log("Server running!")
+});
