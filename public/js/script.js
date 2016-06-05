7fa293a1a167419eb9cbc387c245f45d455bd36e
Maxemilian <maxemiliang@gmail.com>
2016-06-06 00:05:03 +0300

    App is now reversing the array so that they'll show the right way

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ var todos = [];
 var getTodos = $.get('/api/get')
 
 getTodos.done(function(data) {
-  todos = data
+  todos = data.reverse();
   var $ul = $('.todos');
   $ul.empty();
   todos.forEach(function(val, i, todos){
@@ -17,7 +17,7 @@ $('.todo').submit(function( e ) {
   var post = $.post('/api/add' , { text: $('#todoToAdd').val() } );
 
   post.done(function(data) {
-    todos = data
+    todos = data.reverse();
     var $ul = $('.todos');
     $ul.empty();
     todos.forEach(function(val, i, todos){
