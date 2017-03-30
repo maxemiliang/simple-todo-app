00730c96c5f4401a5dd6679bd89f7297e19af6da
Maxemilian Grönblom <maxemiliang@gmail.com>
2017-03-30 10:05:25 +0300

    Possible fix for errors

--- app.js
+++ app.js
@@ -14,15 +14,6 @@ var host = process.env.REDIS_URL || '127.0.0.1';
 
 var client = redis.createClient(host);
 
-limiter({
-	path: '/api/add',
-	method: 'post',
-	lookup: ['connection.remoteAddress'],
-	// 100 requests per 10 mins
-	total: 100,
-	expire: 600000
-})
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -37,6 +28,15 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+limiter({
+	path: '/api/add',
+	method: 'post',
+	lookup: ['connection.remoteAddress'],
+	// 100 requests per 10 mins
+	total: 100,
+	expire: 600000
+})
 
 app.get('/', (req, res) => {
 	res.render('index');
